4826bdc634d12298fdffaebc4f75451e708c0a8f
박종욱 <sa01070@naver.com>
2022-11-24 10:12:50 +0900

    MSG: confirm commit msg

--- backend/ground/src/main/java/com/ground/domain/user/controller/UserController.java
+++ backend/ground/src/main/java/com/ground/domain/user/controller/UserController.java
@@ -61,7 +61,7 @@ public class UserController {
 	@Autowired
 	SocialOauth socialOauth;
 	
-
+    //---------------------------------PJW---------------------
 	
 	@PersistenceContext
 	EntityManager em;
